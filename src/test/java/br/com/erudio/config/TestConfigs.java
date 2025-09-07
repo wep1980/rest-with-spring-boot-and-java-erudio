498c1ccc5d126d35e6bb316cd01802d9195ce629
wepereira <wepereira@tjba.jus.br>
2025-09-06 22:55:53 -0300

    Configuracao para google cloud

--- src/test/java/br/com/erudio/config/TestConfigs.java
+++ src/test/java/br/com/erudio/config/TestConfigs.java
@@ -4,6 +4,7 @@ public interface TestConfigs {
 
     int SERVER_PORT = 8888; // apontando os testes de integração para o container GTI Actions
     //int SERVER_PORT = 80; // apontando os testes de integração para o container local
+    //  int SERVER_PORT = 8080; // apontando os testes de integração para o container local
     String HEADER_PARAM_AUTHORIZATION = "Authorization";
     String HEADER_PARAM_ORIGIN = "Origin";
 
