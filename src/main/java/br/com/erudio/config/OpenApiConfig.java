6281b501b7ada401e530a3dd58cc81f7f0e91bc3
wepereira <wepereira@tjba.jus.br>
2025-09-10 15:40:14 -0300

    Iniciando a verão 1 continuos deployment kubernetes google cloud

--- src/main/java/br/com/erudio/config/OpenApiConfig.java
+++ src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -1,8 +1,10 @@
 package br.com.erudio.config;
 
+import br.com.erudio.service.InstanceInformationService;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,11 +17,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
+    @Autowired
+    InstanceInformationService service;
+
     @Bean
     OpenAPI customOpenAPI(){
         return new OpenAPI()
                 .info(new Info()
-                        .title("REST API's RESTful do zero com Java, Spring Boot, Kubernetes and Docker")
+                        .title("REST API's RESTful do zero com Java, Spring Boot, Kubernetes and Docker V1 " +  service.retrieveInstanceInfo())
                         .version("v1")
                         .description("REST API's RESTful do zero com Java, Spring Boot, Kubernetes and Docker")
                         .termsOfService("https://pub.erudio.com.br/meus-cursos")
